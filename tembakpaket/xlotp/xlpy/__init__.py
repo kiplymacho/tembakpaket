e3c1c43f8d7124cb16a0c4215f560ea7af96692d
kiplymacho <kiplymacho@gmail.com>
2024-06-09 16:28:19 +0800

    tembakpaket

---
+++ tembakpaket/xlotp/xlpy/__init__.py
@@ -0,0 +1,10 @@
+from .config import Config
+from .client import XL
+
+__copyright__       = 'Copyright 2018 by Bintang Nur Pradana'
+__version__         = '5.0.0'
+__author__          = 'Bintang Nur Pradana'
+__author_email__    = 'bintangnurpradana54@gmail.com'
+__url__             = 'https://github.com/Bintang73/tembaktembakan.git'
+
+__all__ = ['Config','XL']
